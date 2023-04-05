792ff6b4aa4b26aa01c3559dff5fafdb40b29dea
ZDSRepositories <89665494+ZDSRepositories@users.noreply.github.com>
2023-04-04 21:41:54 -0500

    Add login checks for existing user and correct password

--- CacheCardsPrototype/loginPage.cs
+++ CacheCardsPrototype/loginPage.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.Windows.Forms.DataFormats;
-using System.Text.Json;
-using System.Configuration;
-using System.IO;
-using System.Collections;
+﻿using System.Text.Json;
 
 namespace CacheCardsPrototype
 {
@@ -19,37 +6,51 @@ namespace CacheCardsPrototype
     {
         public string username { get; set; }
         public string password { get; set; }
-        public User(string usr, string pwd) {
-            username = usr;
-            password = pwd;
+        public User() {
         
         }
     }
     public class DB
     {
-        public Dictionary<string, User> users = new Dictionary<string, User>();
+        public Dictionary<string, User> users { get; set; } = new Dictionary<string, User>();
     }
     public partial class loginPage : Form
     {
         public loginPage()
         {
             InitializeComponent();
-            this.Load += loginPage_Load;
+            this.Load += LoginPage_Load;
         }
 
-        private DB mainDB = new DB();
-        private const string db_filename = "database.json";
+        public DB mainDB = new DB();
+        private const string db_filename = "C:\\Users\\1zada\\OneDrive\\Documents\\software_engineering_1\\cache_cards\\repository\\cache-cards-impl\\CacheCardsPrototype\\database.json";
 
-        private void loginPage_Load(object sender, EventArgs e)
+        private DB deserializeDB()
+        {
+            //User foo = new User();
+            //foo.username = "bob"; foo.password= "tomato";
+            //mainDB.users.Add(foo.username,  foo);
+            //string jsonString = JsonSerializer.Serialize(mainDB);
+            //File.WriteAllText(db_filename, jsonString);
+
+            string jsonString = File.ReadAllText(db_filename);
+            //MessageBox.Show(jsonString);
+            mainDB = JsonSerializer.Deserialize<DB>(jsonString);
+            return mainDB;
+        }
+
+        private void LoginPage_Load(object sender, EventArgs e)
         {
             // Deserialize the DB on startup.
-            string jsonString = File.ReadAllText(db_filename);
-            mainDB = JsonSerializer.Deserialize<DB>(jsonString);            
+            mainDB = deserializeDB();
+            
 
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
+            mainDB = deserializeDB();
             if (!mainDB.users.ContainsKey(textBox1.Text.ToString())) // if there is no user with the username
             {
                 MessageBox.Show("There's no user named '"+ textBox1.Text + "'!");
