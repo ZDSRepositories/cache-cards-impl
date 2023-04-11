9e3049a2171f1f8996a775e3dbfafb3049fc2ec5
jdhs2001 <jspalding369@gmail.com>
2023-04-11 15:13:13 -0500

    moved around some auto generated code.

--- CacheCardsPrototype/loginPage.Designer.cs
+++ CacheCardsPrototype/loginPage.Designer.cs
@@ -2,24 +2,6 @@
 {
     partial class loginPage
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -100,6 +82,26 @@
         }
 
         #endregion
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
 
         private PictureBox pictureBox1;
         private TextBox textBox1;
