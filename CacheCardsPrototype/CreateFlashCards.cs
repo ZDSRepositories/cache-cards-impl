2eb897f29ad919483eb5f03b1133c2fc2a5731e2
enkhod-z <128314169+enkhod-z@users.noreply.github.com>
2023-04-14 05:19:54 -0500

    New CreateFlashcards form
    This is just the form for creating flashcards. I am trying to make the buttons function, connect the form to the database, but I messed something up. Ya'll should only see the form that doesn't function. Will try to fix my issue with syncing git to visual studio, it keeps giving me warnings.

---
+++ CacheCardsPrototype/CreateFlashCards.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CacheCardsPrototype
+{
+    public partial class CreateFlashCards : Form
+    {
+        public CreateFlashCards()
+        {
+            InitializeComponent();
+        }
+    }
+}
